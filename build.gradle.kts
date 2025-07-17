c4fff0fdf53d67e7f6635457ee5ad719f783ddf9
ojas <ojas@nivasafinance.com>
2025-07-17 18:09:16 +0530

    Made compatible with vscode

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,9 @@
 plugins {
 	idea
+	id("kotlin-conventions")
+	id("testing-conventions")
+	id("dokka-conventions")
+	id("spring-conventions")
 }
 
 idea {
@@ -11,4 +15,14 @@ tasks {
 	wrapper {
 		distributionType = Wrapper.DistributionType.ALL
 	}
+}
+
+dependencies {
+	implementation(project(":common"))
+	implementation(project(":features:creditbureau"))
+	implementation(project(":features:person"))
+}
+
+tasks.named<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>("compileKotlin") {
+	destinationDirectory.set(file("${layout.buildDirectory}/classes/java/main"))
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,5 @@
 rootProject.name = "navigator"
 
-include("main")
 include("common")
 include(":features:creditbureau")
 include(":features:person")
