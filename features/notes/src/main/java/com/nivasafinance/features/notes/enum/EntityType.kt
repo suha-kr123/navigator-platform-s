c6aaa68082411a38678a174ea2cc179ba80e99ae
Disha <disha@Dishas-MacBook-Pro.local>
2025-09-15 12:26:06 +0530

    notes changes for entity type

--- features/notes/src/main/java/com/nivasafinance/features/notes/enum/EntityType.kt
+++ features/notes/src/main/java/com/nivasafinance/features/notes/enum/EntityType.kt
@@ -2,14 +2,5 @@ package com.nivasafinance.features.notes.enum
 
 enum class EntityType {
     LEAD,
-    PERSON,
-    ADVISOR,
-    TASK,
-    STAGE,
-    DOCUMENT,
-    PAYMENT,
-    INCOME,
-    ADDRESS,
-    APPLICANT,
-    CUSTOM
+    TASK
 }
