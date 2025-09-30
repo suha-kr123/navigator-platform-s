c6f2b2a4708cc03471944dad90cc1aa30a36c677
Disha <disha@Dishas-MacBook-Pro.local>
2025-09-30 16:34:04 +0530

    Fix missing LocalTimeDeserializer import in AppConfig.kt
    - Added back the LocalTimeDeserializer import that was accidentally removed
    - This fixes the compilation error: Unresolved reference 'LocalTimeDeserializer'

--- common/src/main/java/configs/AppConfig.kt
+++ common/src/main/java/configs/AppConfig.kt
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonInclude
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalTimeDeserializer
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalTimeSerializer
