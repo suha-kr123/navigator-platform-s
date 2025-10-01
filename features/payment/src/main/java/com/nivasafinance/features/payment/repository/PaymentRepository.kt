28c3105d6fd7a7882b4931cd54e95b0aa733e45d
ojas <ojas@nivasafinance.com>
2025-10-01 12:20:19 +0530

    removes payment module

--- features/payment/src/main/java/com/nivasafinance/features/payment/repository/PaymentRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.nivasafinance.features.payment.repository
-
-import com.nivasafinance.features.payment.entity.Payment
-import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.stereotype.Repository
-import java.util.UUID
-
-@Repository
-interface PaymentRepository : JpaRepository<Payment, UUID>
