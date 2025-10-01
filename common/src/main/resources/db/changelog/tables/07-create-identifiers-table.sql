0233abc9622b37a62063b5d18ba79db05a8846fd
ojas <ojas@nivasafinance.com>
2025-10-01 13:25:40 +0530

    removes verification_status and notes from identifiers

--- common/src/main/resources/db/changelog/tables/07-create-identifiers-table.sql
+++ common/src/main/resources/db/changelog/tables/07-create-identifiers-table.sql
@@ -3,8 +3,6 @@ CREATE TABLE identifiers
     id                  uuid PRIMARY KEY,
     identifier          varchar(100) NOT NULL,
     type                varchar(100) NOT NULL,
-    verification_status varchar(255),
-    verification_notes  varchar(255),
     data_ext            jsonb,
     created_by          varchar(255),
     created_at          timestamp,
