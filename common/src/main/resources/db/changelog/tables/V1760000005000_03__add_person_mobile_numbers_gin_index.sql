c7f41f4a59bb7956c5ed21cc86c2ed8a07eec4d9
Vineet-Nivasa <vineet@nivasafinance.com>
2025-10-14 11:35:25 +0530

    Move migrations to respecticve feature module

--- common/src/main/resources/db/changelog/tables/V1760000005000_03__add_person_mobile_numbers_gin_index.sql
+++
@@ -1,8 +0,0 @@
--- Add GIN index on person.mobile_numbers JSONB column for faster JSON searches
--- This index will significantly improve performance for queries that search within
--- the mobile_numbers JSONB array, such as LIKE searches on phone numbers
-
-CREATE INDEX IF NOT EXISTS idx_person_mobile_numbers_gin ON person USING GIN (mobile_numbers);
-
--- Add comment to document the purpose of this index
-COMMENT ON INDEX idx_person_mobile_numbers_gin IS 'GIN index on person.mobile_numbers JSONB column for improved performance on JSON-based phone number searches';
