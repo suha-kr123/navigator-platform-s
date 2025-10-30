071b3b099608bbaef7f08efb51e0c15f1aa355a8
ojas <ojas@nivasafinance.com>
2025-10-30 16:24:05 +0530

    adds `GeoData` DTO with Lombok annotations for latitude and longitude properties

---
+++ common/src/main/java/com/nivasafinance/common/dto/GeoData.java
@@ -0,0 +1,15 @@
+package com.nivasafinance.common.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class GeoData {
+    private Double latitude;
+    private Double longitude;
+}
