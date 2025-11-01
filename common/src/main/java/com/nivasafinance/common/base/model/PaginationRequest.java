32f33f3bce54fef850258fc4224f8809e9846358
ojas <ojas@nivasafinance.com>
2025-11-01 08:11:18 +0530

    refactors `PaginationInfo` and `PaginatedResponse` classes to use Lombok annotations for boilerplate code removal

--- common/src/main/java/com/nivasafinance/common/base/model/PaginationRequest.java
+++ common/src/main/java/com/nivasafinance/common/base/model/PaginationRequest.java
@@ -2,8 +2,15 @@ package com.nivasafinance.common.base.model;
 
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.Objects;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class PaginationRequest {
     public static final int DEFAULT_LIMIT = 20;
     public static final long MAX_LIMIT = 100L;
@@ -15,75 +22,7 @@ public class PaginationRequest {
     @Max(value = MAX_LIMIT, message = "Limit cannot exceed " + MAX_LIMIT)
     private int limit = DEFAULT_LIMIT;
 
-    private String sortBy;
-    private String sortDirection = "ASC";
-
-    public PaginationRequest() {
-    }
-
-    public PaginationRequest(int offset, int limit, String sortBy, String sortDirection) {
-        this.offset = offset;
-        this.limit = limit;
-        this.sortBy = sortBy;
-        this.sortDirection = sortDirection != null ? sortDirection : "ASC";
-    }
-
-    public int getOffset() {
-        return offset;
-    }
-
-    public void setOffset(int offset) {
-        this.offset = offset;
-    }
-
-    public int getLimit() {
-        return limit;
-    }
-
-    public void setLimit(int limit) {
-        this.limit = limit;
-    }
-
-    public String getSortBy() {
-        return sortBy;
-    }
-
-    public void setSortBy(String sortBy) {
-        this.sortBy = sortBy;
-    }
-
-    public String getSortDirection() {
-        return sortDirection;
-    }
-
-    public void setSortDirection(String sortDirection) {
-        this.sortDirection = sortDirection;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        PaginationRequest that = (PaginationRequest) o;
-        return offset == that.offset &&
-                limit == that.limit &&
-                Objects.equals(sortBy, that.sortBy) &&
-                Objects.equals(sortDirection, that.sortDirection);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(offset, limit, sortBy, sortDirection);
-    }
-
-    @Override
-    public String toString() {
-        return "PaginationRequest{" +
-                "offset=" + offset +
-                ", limit=" + limit +
-                ", sortBy='" + sortBy + '\'' +
-                ", sortDirection='" + sortDirection + '\'' +
-                '}';
-    }
+    private String sortBy = "created_at";
+    private String sortDirection = "DESC";
 }
 
