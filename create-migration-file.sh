4b2a4a220041c37787b850e1d24932d84b3394e7
ojas <ojas@nivasafinance.com>
2025-11-04 08:50:53 +0530

    updates `create-migration-file.sh` to generate Liquibase XML files with a changeset template, replaces SQL file generation logic, and adjusts default migration directory to current directory for improved usability

--- create-migration-file.sh
+++ create-migration-file.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 REL_DIR=`dirname $0`
-MIGRATION_DIR="common/src/main/resources/db/changelog/tables"
+MIGRATION_DIR="."
 echo $REL_DIR
 if [ $REL_DIR != "." ]  ; then
   echo "This script should be run from the navigator directory, like: ./create-migration-file.sh '<Description_with_underscore_separator>'"
@@ -9,7 +9,11 @@ if [ $REL_DIR != "." ]  ; then
 fi
 
 function generate_migration_name {
-  echo "$MIGRATION_DIR/V`date +%s`000_$(( $RANDOM % 99 ))__`echo "$1" | awk '{print tolower($0)}'`.sql"
+  echo "$MIGRATION_DIR/V`date +%s`000_$(( $RANDOM % 99 ))__`echo "$1" | awk '{print tolower($0)}'`.xml"
+}
+
+function generate_changeset_id {
+  echo "V`date +%s`000_$(( $RANDOM % 99 ))"
 }
 
 if [ "$#" -ne 1 ]; then
@@ -25,8 +29,27 @@ if [ "$#" -ne 1 ]; then
 fi
 
 new_migration=`generate_migration_name $1`
-touch $new_migration
+changeset_id=`generate_changeset_id`
+
+# Create XML file with Liquibase template
+cat > "$new_migration" << EOF
+<?xml version="1.0" encoding="UTF-8" ?>
+<databaseChangeLog
+        xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+        xmlns="http://www.liquibase.org/xml/ns/dbchangelog"
+        xsi:schemaLocation="http://www.liquibase.org/xml/ns/dbchangelog
+                      http://www.liquibase.org/xml/ns/dbchangelog/dbchangelog-3.8.xsd">
+
+    <changeSet id="$changeset_id" author="system" context="default">
+        
+        <!-- Add your database changes here -->
+        
+    </changeSet>
+
+</databaseChangeLog>
+EOF
 
 echo
 echo "Created migration $new_migration"
+echo "Changeset ID: $changeset_id"
 echo
