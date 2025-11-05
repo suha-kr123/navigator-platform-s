591afa6eac0b291e0dea99c4a198f5d44ea22ba7
ojas <ojas@nivasafinance.com>
2025-11-05 11:46:45 +0530

    removes JWT configuration and decoding logic from `SecurityConfig` and application properties; simplifies CORS configuration

--- security/src/main/java/com/nivasafinance/security/config/SecurityConfig.java
+++ security/src/main/java/com/nivasafinance/security/config/SecurityConfig.java
@@ -1,23 +1,16 @@
 package com.nivasafinance.security.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.oauth2.jose.jws.SignatureAlgorithm;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-
-    @Value("${spring.security.oauth2.resourceserver.jwt.jwk-set-uri:}")
-    private String jwkSetUri;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -27,13 +20,6 @@ public class SecurityConfig {
                         .anyRequest().permitAll()
                 );
         return http.build();
-    }
-
-    @Bean
-    public JwtDecoder nimbusJwtDecoder() {
-        return NimbusJwtDecoder.withJwkSetUri(jwkSetUri)
-                .jwsAlgorithm(SignatureAlgorithm.ES256)
-                .build();
     }
 }
 
