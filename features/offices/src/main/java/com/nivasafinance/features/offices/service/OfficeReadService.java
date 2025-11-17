778aaaecd0157d2b6482ddfa939c76403caa5ee1
ojas <ojas@nivasafinance.com>
2025-11-17 14:29:31 +0530

    Adds `getOfficeByKeys` method to `OfficeReadService` and implementation in `OfficeReadServiceImpl` to fetch multiple office details by keys.

--- features/offices/src/main/java/com/nivasafinance/features/offices/service/OfficeReadService.java
+++ features/offices/src/main/java/com/nivasafinance/features/offices/service/OfficeReadService.java
@@ -4,8 +4,12 @@ import com.nivasafinance.common.base.model.PaginatedResponse;
 import com.nivasafinance.common.base.model.PaginationRequest;
 import com.nivasafinance.features.offices.dto.OfficeResponse;
 
+import java.util.List;
+
 public interface OfficeReadService {
     OfficeResponse getOfficeByKey(String key);
+
+    List<OfficeResponse> getOfficeByKeys(List<String> keys);
 
     PaginatedResponse<OfficeResponse> getOffices(String parentKey, String nameQuery, PaginationRequest paginationRequest);
 }
