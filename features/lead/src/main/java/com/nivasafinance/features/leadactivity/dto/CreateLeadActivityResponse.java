232d5124a6e1befc59dfc6c017bbfb65281e4c65
ojas <ojas@nivasafinance.com>
2025-11-19 22:05:49 +0530

    Adds `id` field to `LeadCreationEventPayload` and populates it in `LeadWriteServiceImpl`.

---
+++ features/lead/src/main/java/com/nivasafinance/features/leadactivity/dto/CreateLeadActivityResponse.java
@@ -0,0 +1,17 @@
+package com.nivasafinance.features.leadactivity.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class CreateLeadActivityResponse {
+    private Long id;
+    private UUID identifier;
+}
