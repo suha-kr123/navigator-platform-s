0e216d362c95b5693bdae3a98384297b1c85ab80
Disha <disha@Dishas-MacBook-Pro.local>
2025-11-27 16:14:21 +0530

    exception handling

--- features/person/src/main/java/com/nivasafinance/features/person/exception/PersonOperationException.java
+++ features/person/src/main/java/com/nivasafinance/features/person/exception/PersonOperationException.java
@@ -1,11 +1,12 @@
 package com.nivasafinance.features.person.exception;
 
+import com.nivasafinance.common.exception.BadRequestException;
 import com.nivasafinance.common.exception.ExceptionUtils;
 import org.springframework.context.MessageSource;
 
 import java.io.Serial;
 
-public class PersonOperationException extends RuntimeException {
+public class PersonOperationException extends BadRequestException {
     @Serial
     private static final long serialVersionUID = 882423L;
     public PersonOperationException(String messageCode, MessageSource messageSource) {
