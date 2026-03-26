07548af6d074bece0a5b50ab9d6c3b1e3617226d
Likitha-KM <likitha@nivasafinance.com>
2026-03-26 18:11:12 +0530

    handle LEAD_CALL_LOG_CREATED for Atlas

--- features/atlas/src/main/java/com/nivasafinance/features/atlas/service/AtlasService.java
+++ features/atlas/src/main/java/com/nivasafinance/features/atlas/service/AtlasService.java
@@ -1,8 +1,11 @@
 package com.nivasafinance.features.atlas.service;
 
+import com.nivasafinance.common.events.payload.LeadCallLogCreationEventPayload;
 import com.nivasafinance.common.events.payload.LeadCallLogUpdateEventPayload;
 
 public interface AtlasService {
+
+    void handleLeadCallLogCreated(LeadCallLogCreationEventPayload payload);
 
     void handleLeadCallLogUpdated(LeadCallLogUpdateEventPayload payload);
 }
