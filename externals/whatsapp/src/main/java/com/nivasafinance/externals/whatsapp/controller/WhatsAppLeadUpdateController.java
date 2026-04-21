38a9eeea28918ff91a97d4ea6a4ed407df6465aa
Bhaskara <bhaskara@nivasafinance.com>
2026-04-21 18:29:28 +0530

    Add WhatsApp external lead PATCH update API for onhold update

---
+++ externals/whatsapp/src/main/java/com/nivasafinance/externals/whatsapp/controller/WhatsAppLeadUpdateController.java
@@ -0,0 +1,25 @@
+package com.nivasafinance.externals.whatsapp.controller;
+
+import com.nivasafinance.common.constants.ApiConstants;
+import com.nivasafinance.externals.whatsapp.dto.WhatsAppLeadUpdateRequest;
+import com.nivasafinance.externals.whatsapp.service.WhatsAppLeadUpdateService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(ApiConstants.EXTERNAL_V1 + "/whatsapp")
+@AllArgsConstructor
+public class WhatsAppLeadUpdateController {
+
+    private final WhatsAppLeadUpdateService whatsAppLeadUpdateService;
+
+    @PatchMapping("/leads/update")
+    public ResponseEntity<Void> updateLead(@RequestBody WhatsAppLeadUpdateRequest request) {
+        whatsAppLeadUpdateService.updateLead(request);
+        return ResponseEntity.noContent().build();
+    }
+}
